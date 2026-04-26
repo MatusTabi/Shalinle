75bca564b85b22fcd4a467ba7e0a1b5fe2ffc041
MatusTabi <matustabi44@gmail.com>
2026-04-26 22:51:51 +0200

    feat: update Home component to include Select for stop selection

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,15 +1,18 @@
+import Select from '../components/select';
 import { useStopsQuery } from '../hooks/stops';
 
 export const Home = () => {
-    console.log('Rendering Home component...');
     const { isLoading, data, error } = useStopsQuery();
 
     return (
         <>
-            <h1 className="text-3xl">Hi there!</h1>
             {isLoading && <p>Loading...</p>}
-            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
             {error && <p>Error: {error.message}</p>}
+            <Select
+                options={data?.map((stop) => stop.name) || []}
+                onChange={(value) => console.log(value)}
+                value=""
+            />
         </>
     );
 };
